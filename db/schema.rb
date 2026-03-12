9a31afc21a95f6975ae432e817a83add4bef88f1
tofu-dev0123 <m.komukai0123@gmail.com>
2026-03-12 21:57:41 +0900

    マイグレーションの更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_09_125840) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_12_120000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -111,7 +111,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_09_125840) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "avatar_url", limit: 2048
+    t.string "avatar_key"
     t.text "bio"
     t.string "clerk_user_id", limit: 50, null: false
     t.datetime "created_at", null: false
